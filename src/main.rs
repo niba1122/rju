2b0f746f3584e8821911e98287c23db93836977d
Nobuhito Ibaraki <niba1122@gmail.com>
2017-11-15 22:18:17 +0900

    before new component

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,59 @@ use rju_macro::{html};
 
 use std::cell::RefCell;
 
+use std::sync::RwLock;
+
+#[macro_use]
+extern crate lazy_static;
+
+fn add() {
+    // let mut h = hoge.write().unwrap();
+    // *h += 1;
+    component.update();
+}
+
+fn render() -> VirtualDOM {
+    let hoge2 = *hoge.read().unwrap();
+    let hoge_string = hoge2.to_string();
+    html!(r#"
+        <div>
+            <strong bind:class='hoge2.to_string()'>
+                Hello World!!!!!!
+            </strong>
+            <p>
+                nemukunaiyo!!!!!!!!!!!
+            </p>
+            <button on:click="add">Add</button>
+        </div>
+    "#)
+}
+
+lazy_static! {
+    static ref component: Component = {
+        let mut c = Component {
+            parent_dom_id: "test",
+            render: render,
+        };
+        c
+    };
+    static ref state: State = {
+        let mut s = State {
+            hoge: "aiueo".to_string(),
+            fuga: 0,
+            foo: RwLock::new(vec![])
+        };
+        s
+    };
+    static ref hoge: RwLock<i32> = {
+        let mut m = 0;
+        RwLock::new(m)
+    };
+}
 
 struct State {
     hoge: String,
-    fuga: i32
+    fuga: i32,
+    foo: RwLock<Vec<i32>>
 }
 
 impl State {
@@ -19,39 +68,21 @@ impl State {
     }
 }
 
-fn test() {
-    println!("print!!!!!!!!!!!!!!!!!");
-}
-
 fn main() {
-    let component = Component {
-        parent_dom_id: "test",
-        render: |component| -> VirtualDOM {
-            html!(r#"
-                <div>
-                    <strong bind:class='"foobar".to_string()'>
-                        Hello World!!!!!!
-                    </strong>
-                    <p>
-                        nemuiyo!!!!!!!!!!!
-                    </p>
-                    <button on:click="test">add</button>
-                </div>
-            "#)
-        },
-    };
-    component.update()
+    Renderer::initialize();
+    component.update();
+    Renderer::start();
 }
 
 
 #[no_mangle]
 pub extern fn sum(a: i32, b: i32) -> i32 {
-    println!("success: {}", html!("
-        <html>
-            <body>
-            <h1>Hello World</h1>
-        </body>
-      </html>
-    "));
+    // println!("success: {}", html!("
+    //     <html>
+    //         <body>
+    //         <h1>Hello World</h1>
+    //     </body>
+    //   </html>
+    // "));
     a * b
 }
