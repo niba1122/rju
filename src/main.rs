8e99d7efc0d6632ed6e6eaade39ef0bd40d29e6f
Nobuhito Ibaraki <niba1122@gmail.com>
2017-11-19 13:49:13 +0900

    new component

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,36 @@
 extern crate rju;
 extern crate rju_macro;
 
-use rju::{h, Renderer, DOMType, Attribute, Component, VirtualDOM, ComponentFactory};
+use rju::{h, Renderer, DOMType, Attribute, Component, VirtualDOM};
 use rju_macro::{html};
 
 
 #[macro_use]
 extern crate lazy_static;
 
-#[derive(Hash)]
-pub struct OriginalComponent {
-    id: i32,
+pub fn render(id: i32) -> VirtualDOM {
+    let hoge = "hogestring".to_string();
+    html!(r#"
+        <div>
+            <h1 bind:class='hoge'>
+                Hello World!
+            </h1>
+            <p>
+                new component system!
+            </p>
+        </div>
+    "#)
 }
 
-impl Component for OriginalComponent {
-    fn render(&self) -> VirtualDOM {
-        let hoge = &self;
-        html!(r#"
-            <div>
-                <h1 bind:class='self.id.to_string()'>
-                    Hello World!
-                </h1>
-                <p>
-                    new component system!
-                </p>
-                <button on:click='Box::new(||{println!("clicked!!!!!");})'>Add</button>
-            </div>
-        "#)
-    }
-}
-
-pub struct OriginalComponentFactory {}
-impl ComponentFactory<OriginalComponent> for OriginalComponentFactory {
-    fn create() -> OriginalComponent {
-        OriginalComponent{id: 10}
+fn factory() -> Component {
+    Component {
+        render: render
     }
 }
 
 fn main() {
-    Renderer::render("test", OriginalComponentFactory::create);
+    // Renderer::render("test", OriginalComponentFactory::create);
+    Renderer::render("test", factory)
 }
 
 #[no_mangle]
