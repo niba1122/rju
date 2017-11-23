582f5396c1cf3d42142da70cc1923bf0920f33ea
Nobuhito Ibaraki <niba1122@gmail.com>
2017-11-23 15:38:53 +0900

    state of user defined type

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,29 @@
 extern crate rju;
 extern crate rju_macro;
 
-use rju::{h, Renderer, DOMType, Attribute, Component, VirtualDOM};
+use rju::{h, Renderer, DOMType, Attribute, Component, VirtualDOM, State};
 use rju_macro::{html};
 
-pub use std::sync::Mutex;
-pub use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::Arc;
+use std::any::Any;
 
 #[macro_use]
 extern crate lazy_static;
 
+struct MainState {
+    count: i32
+}
+
+impl State for MainState {
+    fn as_any(&self) -> &Any {
+        self
+    }
+}
+
 pub fn render(component: Arc<Mutex<Component>>) -> VirtualDOM {
     let hoge = "hogestring".to_string();
-    let current_state = component.lock().unwrap().state;
+    let current_state: i32 = component.lock().unwrap().state.as_any().downcast_ref::<MainState>().unwrap().count;
     html!(r#"
         <div>
             <h1 bind:class='current_state.to_string()'>
@@ -29,20 +40,23 @@ pub fn render(component: Arc<Mutex<Component>>) -> VirtualDOM {
 }
 
 fn handle_click(component: Arc<Mutex<Component>>) {
-    let current_state = component.lock().unwrap().state;
-    component.lock().unwrap().set_state(current_state + 1);
+    let current_state = component.lock().unwrap().state.as_any().downcast_ref::<MainState>().unwrap().count;
+    component.lock().unwrap().set_state(Box::new(MainState {
+        count: current_state + 1
+    }));
     component.lock().unwrap().update();
 }
 
 fn factory() -> Component {
     Component {
         render: render,
-        state: 0
+        state: Box::new(MainState {
+            count: 0
+        })
     }
 }
 
 fn main() {
-    // Renderer::render("test", OriginalComponentFactory::create);
     Renderer::render("test", factory)
 }
 
